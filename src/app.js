f1cea0ebafe1abd0e85b389a65936a2325c69a06
Rupert <zoltan.szokodi@hotmail.com>
2019-05-25 15:38:59 +0200

    Add Posts & Show Alert

--- src/app.js
+++ src/app.js
@@ -2,15 +2,40 @@
 import { EasyHTTP } from './http';
 import { UI } from './ui';
 
-// Instanciate a new instances
+// Instantiate a new instances
 const http = new EasyHTTP();
 const ui = new UI();
 
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
 function getPosts() {
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
   .catch(err => console.log(err));
+}
+
+// Submit Post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  // Create Post
+  http.post('http://localhost:3000/posts', data)
+  .then(data => {
+    ui.showAlert('Post added', 'alert alert-success');
+    ui.clearFields();
+    getPosts();
+  })
+  .catch(err => {
+    console.log(err)
+  });
 }
