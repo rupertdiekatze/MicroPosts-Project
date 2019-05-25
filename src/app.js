4ef3f2834dd4123f2d0c4cacaae2fa921f6aeb59
Rupert <zoltan.szokodi@hotmail.com>
2019-05-25 16:21:56 +0200

    Removing Posts

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 function getPosts() {
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
@@ -38,4 +41,21 @@ function submitPost() {
   .catch(err => {
     console.log(err)
   });
+}
+
+// Delete Post
+function deletePost(e) {
+  e.preventDefault();
+  
+  if(e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+      .then(data => {
+        ui.showAlert('Post Removed', 'alert alert-success');
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    }
+  }
 }
